4fd7df9cbb2716ea5acd9b1bb717d99a298a293f
Noah Schill <noahrschill@gmail.com>
2021-02-03 20:58:33 -0700

    oh no

--- src/hangman/EvilHangman.java
+++ src/hangman/EvilHangman.java
@@ -1,9 +1,71 @@
 package hangman;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class EvilHangman {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, EmptyDictionaryException, GuessAlreadyMadeException {
+        try {
+            EvilHangmanGame game = new EvilHangmanGame();
+            while (!game.isOver) {
+                //Checks to make sure we have sufficient guesses left
+                if (game.guessesLeft == 0) {
+                    System.out.println("You lose! The word was: " + game.wordSet.iterator().next());
+                    game.isOver = true;
+                    break;
+                }
 
+                try {
+                    char userChoice = displayAndPrompt(game);
+                    SortedSet guess = (TreeSet) game.makeGuess(userChoice);
+
+                    if (guess.size() > 0) {
+                        game.wordSet = (TreeSet<String>) guess;
+                        System.out.println("Yes, there are " + userChoice); //TODO: show how many of the guessed there are
+                        game.guessesLeft--;
+                    }
+                    if (guess.size() <= 0) {
+                        System.out.println("No, there is no " + userChoice);
+                        game.guessesLeft--;
+                    }
+
+                    game.checkWin();
+
+                } catch (IOException e){
+                    System.out.println("Invalid input type");
+                }
+
+                System.out.println("------------");
+            }
+        } catch (IOException | EmptyDictionaryException e) {
+            e.printStackTrace();
+        }
     }
 
+    private static char displayAndPrompt(EvilHangmanGame game) throws IOException, GuessAlreadyMadeException{
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        char userGuess = '-';
+        System.out.println("You have " + game.guessesLeft + " guesses left");// Enter username and press Enter
+
+        if (game.getGuessedLetters() != null) {
+            System.out.print("Used letters:");
+            for (char c : game.getGuessedLetters()){
+                System.out.print(c + " ");
+            }
+            System.out.println("");
+        } else {
+            System.out.println("Used letters:");
+        }
+        System.out.println("Word: " + game.getStringRep());
+        System.out.print("Enter guess: ");
+        userGuess = scanner.nextLine().toLowerCase().charAt(0);
+        if (!Character.isLetter(userGuess)){
+            throw new IOException("Invalid input type");
+        }
+        game.previousGuess = userGuess;
+        return userGuess;
+    }
 }
--- src/hangman/EvilHangmanGame.java
+++ src/hangman/EvilHangmanGame.java
@@ -1,11 +1,12 @@
 package hangman;
 
+import com.sun.source.tree.Tree;
+
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class EvilHangmanGame implements IEvilHangmanGame {
 
@@ -14,54 +15,197 @@ public class EvilHangmanGame implements IEvilHangmanGame {
     boolean isOver;
     SortedSet<Character> guessedLetters;
     TreeSet<String> wordSet;
+    char previousGuess;
+    private String minCharSetStr;
+    private String currentPattern;
+
 
     public EvilHangmanGame() throws IOException, EmptyDictionaryException {
-        startGame(new File("small.txt"),5);
+        startGame(new File("dictionary.txt"), 3);
     }
+
 
     @Override
     public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
+        for (int i = 0; i < wordLength; i++){
+            currentPattern = currentPattern+"-";
+        }
         this.guessesLeft = 10;
+        this.guessedLetters = new TreeSet<>();
         this.isOver = false;
         this.wordLength = wordLength;
         this.wordSet = new TreeSet<>();
         Scanner scanner = new Scanner(dictionary);
-        while (scanner.hasNext()) {
-            String line = scanner.next();
-            if(line.length() == wordLength){
-                wordSet.add(line.toLowerCase());
-            }
+        if (!scanner.hasNext()){
+            throw new EmptyDictionaryException();
         }
-
-        while(!this.isOver){
-            //Checks to make sure we have sufficient guesses left
-            if(this.guessesLeft < 1){
-                isOver = true;
+        while (scanner.hasNext()) { //TODO: Handle dictionary exceptions by spec
+            String line = scanner.next();
+            if (line.length() == wordLength) {
+                this.wordSet.add(line.toLowerCase());
             }
 
-            Scanner in = new Scanner(System.in);
-            String userChoice = in.next();
-            if(userChoice.length() == 1){
-                    try {
-                        this.wordSet = (TreeSet<String>) makeGuess(userChoice.charAt(0));
-                    } catch (GuessAlreadyMadeException e) {
-                        throw new IOException("Guess already made");
-                    }
-            } else {
-                throw new IOException("Incorrect input length");
+        }
+        if (wordSet.size() == 0){
+            throw new EmptyDictionaryException();
+        }
+    }
+
+    public void checkWin() {
+        if (this.wordSet.size() == 1) { //Checks for unlikely win
+            boolean containsAllLetters = true;
+            for (char c : this.guessedLetters){
+                String s = "" + c;
+                if(!this.wordSet.iterator().next().contains(s)){
+                    containsAllLetters = false;
+                }
+            }
+            if (containsAllLetters) {
+                System.out.println("You win! The word was: " + this.wordSet.iterator().next());
+                this.isOver = true;
             }
         }
     }
 
     @Override
-    public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
-        //TODO: Implement partitioning
-        //TODO: Implement Evil Algorithm
-        return null;
+    public Set<String> makeGuess(char g) throws GuessAlreadyMadeException {
+        TreeMap<String, TreeSet<String>> partitions = new TreeMap<>();
+
+        String s = g + "";
+        s = s.toLowerCase();
+        char guess = s.charAt(0);
+
+        //Check for guess already made
+        if(this.guessedLetters.contains(guess) || this.guessedLetters.contains(g)){
+            throw new GuessAlreadyMadeException();
+        }
+
+        for (String word : this.wordSet) {
+            String key = getSubsetKey(word, guess);
+            TreeSet<String> currentSubset = new TreeSet<>();
+                if (partitions.get(key) == null) {
+                    currentSubset.add(word);
+                    partitions.put(key, currentSubset);
+                } else {
+                    //Update, add word to the set
+                    partitions.get(key).add(word);
+                }
+        }
+
+
+        guessedLetters.add(guess);
+        this.wordSet = getMaxPartition(partitions, guess);
+        return wordSet;
+    }
+
+    private TreeSet<String> getMaxPartition(TreeMap<String, TreeSet<String>> partitions, char guess) {
+        Map<String, TreeSet<String>> subPartitions = new TreeMap<>();
+        int maxSize = 0;
+        for (Map.Entry<String, TreeSet<String>> entry : partitions.entrySet()) {
+            if(entry.getValue().size() > maxSize) {
+                subPartitions.clear();
+                subPartitions.put(entry.getKey(), entry.getValue());
+                maxSize = entry.getValue().size();
+                currentPattern = entry.getKey();
+            } else if (entry.getValue().size() == maxSize) {
+                subPartitions.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        if (subPartitions.size() > 1) {
+            subPartitions = getPartitionWithFewestInstances(partitions);
+        }
+
+        if (subPartitions.size() > 1) {
+            subPartitions = getRightmostPartition(partitions);
+        }
+
+        return subPartitions.get(currentPattern);
+    }
+
+    private TreeMap<String, TreeSet<String>> getPartitionWithFewestInstances(TreeMap<String, TreeSet<String>> partitions) {
+        TreeMap<String, TreeSet<String>> partitionWithFewestInstances = new TreeMap<>();
+        int maxCount = 0;
+        for (Map.Entry<String, TreeSet<String>> entry : partitions.entrySet()) {
+            int dashCount = 0;
+            for (int i = 0; i < entry.getKey().length(); i++) {
+                if (entry.getKey().charAt(i) == '-') {
+                    dashCount++;
+                }
+            }
+            if (dashCount > maxCount) {
+                partitionWithFewestInstances.clear();
+                partitionWithFewestInstances.put(entry.getKey(), entry.getValue());
+                maxCount = dashCount;
+                currentPattern = entry.getKey();
+
+            } else if (dashCount == maxCount){
+                partitionWithFewestInstances.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        return partitionWithFewestInstances;
+    }
+
+    private TreeMap<String, TreeSet<String>> getRightmostPartition(TreeMap<String, TreeSet<String>> partitions) {
+        TreeMap<String, TreeSet<String>> rightmostPartition = new TreeMap<>();
+        int maxDashCount = 0;
+        for (Map.Entry<String, TreeSet<String>> entry : partitions.entrySet()) {
+            int dashCount = 0;
+            for (int i = 0; i < entry.getKey().length(); i++) {
+                if (entry.getKey().charAt(i) == '-') {
+                    dashCount++;
+                } else {
+                    break;
+                }
+            }
+
+            if (dashCount > maxDashCount) {
+                rightmostPartition.clear();
+                rightmostPartition.put(entry.getKey(), entry.getValue());
+                maxDashCount = dashCount;
+                currentPattern = entry.getKey();
+            }
+
+            else if (dashCount == maxDashCount){
+//                rightmostPartition.clear();
+                rightmostPartition.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return rightmostPartition;
     }
 
     @Override
     public SortedSet<Character> getGuessedLetters() {
         return guessedLetters;
     }
+
+    private String getSubsetKey(String word, char guessedLetter) {
+        char[] key = word.toCharArray();
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != guessedLetter && !guessedLetters.contains(guessedLetter)) {
+                key[i] = '-';
+            }
+        }
+        return new String(key);
+    }
+
+
+    public String getStringRep() {
+
+        String word = currentPattern;
+        char[] key = word.toCharArray();
+
+        //populate out with dashes to avoid index errors
+        for (char c : guessedLetters) {
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) != c) {
+                    key[i] = '-';
+                }
+            }
+        }
+
+        return Arrays.toString(key).replace("]","").replace("[","");
+    }
+
 }
